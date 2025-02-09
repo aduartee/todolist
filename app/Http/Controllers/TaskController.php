bc35bdf53b5390736ebdd86a2c0f4a079f36f1c5
aduartee <arthurduartealves99@gmail.com>
2025-02-09 17:07:31 -0300

    feat: Create the function to register tasks in the database

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,32 +5,48 @@ namespace App\Http\Controllers;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use App\Models\Task;
+use Dotenv\Exception\ValidationException;
 
 class TaskController extends Controller
 {
     public function createNewTask(Request $request): JsonResponse
-    {   
-        error_log($request->get('name'));
-        
-        return response()->json([
-            'message', 'teste',
-            'testField' => $request
-        ], 200);
+    {
+        error_log($request);
+
+        try {
+            $dataValidate = $request->validate(
+                [
+                    'name' => 'required|string|max:255'
+                ]
+            );
+
+            Task::create($dataValidate);
+
+            return response()->json([
+                'message' => 'success',
+                'requestData' => $dataValidate
+            ], 200);
+        } catch (ValidationException $validationException) {
+            return response()->json([
+                'title' => 'error',
+                'message' => 'Error to register a new task. Review the fields and try again.'
+            ], 422);
+        }
     }
 
     public function fetchAllTasks(): JsonResponse
     {
         $dataFromDatabase = Task::all();
-        if($dataFromDatabase->isEmpty()) {
+        if ($dataFromDatabase->isEmpty()) {
             return response()->json([
                 'message' => 'error',
                 'data' => []
             ], 404);
         }
-        
+
         return response()->json([
             'message' => 'success',
-            'data' => $dataFromDatabase, 
+            'data' => $dataFromDatabase,
         ], 200);
     }
 }
